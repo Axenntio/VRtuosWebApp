f73a1eeb20bdc150774ba1f460abeb31dbcffc90
Axenntio <axenntio@live.com>
2021-03-03 19:16:02 +0100

    Added missing channels message type

--- scripts/keyboard.js
+++ scripts/keyboard.js
@@ -23,16 +23,46 @@ keyboard = {
 		}
 	
 		switch (command) {
+			case 128: // note off
+			case 129:
+			case 130:
+			case 131:
+			case 132:
+			case 133:
+			case 134:
+			case 135:
+			case 136:
+			case 137:
+			case 138:
+			case 139:
+			case 140:
+			case 141:
+			case 142:
+			case 143:
+				keyboardNote.classList.remove(from)
+				break;
+
 			case 144: // note on
+			case 145:
+			case 146:
+			case 147:
+			case 148:
+			case 149:
+			case 150:
+			case 151:
+			case 152:
+			case 153:
+			case 154:
+			case 155:
+			case 156:
+			case 157:
+			case 158:
+			case 159:
 				if (velocity > 0) {
 					keyboardNote.classList.add(from)
 				} else {
 					keyboardNote.classList.remove(from)
 				}
-				break;
-	
-			case 128: // note off
-				keyboardNote.classList.remove(from)
 				break;
 			
 			default:
