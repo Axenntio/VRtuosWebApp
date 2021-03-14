9602e3004d9d8e8f50dc6cf61795d3b97ce79779
Axenntio <axenntio@live.com>
2021-03-14 11:03:49 +0100

    Updated port number

--- testServer/server.cs
+++ testServer/server.cs
@@ -7,7 +7,7 @@ using System.Text.RegularExpressions;
 class Server {
     public static void Main() {
         string ip = "127.0.0.1";
-        int port = 42096;
+        int port = 42069;
         var server = new TcpListener(IPAddress.Parse(ip), port);
 
         server.Start();
